ff3fc38a18ee0e09e41696bcd24236c5c5f01bf0
Kadyr Sherfedinov <kadyr.sherfedinov@netcracker.com>
2021-03-19 21:21:39 +0300

    initial commit

---
+++ src/lecture0/Program.java
@@ -0,0 +1,11 @@
+package lecture0;
+
+public class Program {
+
+    public static void main(String[] args) {
+        int a = 12;
+        double b = 5.6;
+        byte v = (byte) (a + b);
+        System.out.println(v);
+    }
+}
